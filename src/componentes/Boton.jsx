6191e3c6359b03feb5d09011ba4d0a44f327c98d
fredfrontend <fdsdesignstudio5@gmail.com>
2022-10-29 00:33:33 -0300

    first upload repository :D

---
+++ src/componentes/Boton.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Boton.css';
+
+const Boton = (props) => {
+    const esOperador = valor => {
+        return isNaN(valor) && (valor !== '.') && (valor !== '=');
+    }
+  return (
+    <div
+    className={`boton-contenedor ${esOperador(props.children) ? 'operador' : ''}`.trimEnd()}
+    onClick={() => props.manejarClic(props.children)}>
+        {props.children}
+    </div>
+  )
+}
+
+export default Boton
---
+++ src/componentes/Display.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Display.css';
+
+const Display = ({input}) => (
+  <div className='display'>{input}</div>
+);
+
+export default Display;
